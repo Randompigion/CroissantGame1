74db875f93fb2304c527e429315f26ed97d20675
Randompigion <aahilahsan@outlook.com>
2026-06-28 20:15:46 -0400

    polished

--- croissant-game/cat_banana.gd
+++ croissant-game/cat_banana.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 15.0
 const JUMP_VELOCITY = 20
-
+var can_animate = true
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,7 +16,7 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_pressed("Down"):
 		velocity.z = 20
 	else:
-		velocity.z = 25
+		velocity.z = 30
 		
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
@@ -24,15 +24,19 @@ func _physics_process(delta: float) -> void:
 	
 	if is_on_floor():
 		if Input.is_action_pressed("Duck"):
-			$catBananaLOW/AnimationPlayer.play("Slide")
 			%catBananaCollsion.scale = Vector3(1,0.5,1)
+			if can_animate == true:
+				$catBananaLOW/AnimationPlayer.play("Slide")
 		elif Input.is_action_pressed("Up"):
-			$catBananaLOW/AnimationPlayer.play("run")
+			if can_animate == true:
+				$catBananaLOW/AnimationPlayer.play("run")
 		else:
-			$catBananaLOW/AnimationPlayer.play("walk")
 			%catBananaCollsion.scale= Vector3(1,1,1)
+			if can_animate == true:
+				$catBananaLOW/AnimationPlayer.play("walk")
 	else:
-		$catBananaLOW/AnimationPlayer.play("Jump")
+		if can_animate == true:
+			$catBananaLOW/AnimationPlayer.play("Jump")
 	
 
 		
@@ -45,6 +49,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
 
-
-func _on_kill_box_body_entered(body: Node3D) -> void:
-	pass # Replace with function body.
+func _on_winbox_body_entered(body: Node3D) -> void:
+	can_animate = false
+	$catBananaLOW/AnimationPlayer.play("Win")
+	await $catBananaLOW/AnimationPlayer.animation_finished
+	get_tree().paused = true
